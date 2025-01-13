d58dd22f9cb72d34473e6f0004528f9b132c84bf
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-13 10:14:34 -0700

    BOUNCING

--- bouncing/bounce.py
+++ bouncing/bounce.py
@@ -1,7 +1,7 @@
 import pygame as py
 
 # Initialize Engine
-py.init
+py.init()
 
 # Variable Creation
 BLACK = (0, 0, 0)
@@ -10,28 +10,56 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
+# Image Load
+prisonMike = py.image.load("mikey.jpg")
+prisonMike.set_colorkey(BLACK)
+resized_mike = py.transform.scale(prisonMike, [50,50])
+
+# Audio loading
+bounceSound = py.mixer.Sound("homer.mp3")
+py.mixer.music.load("never-1.mp3")
+
 # Set up the screen
-SIZE = [400, 400]
+SIZE = [1200, 600]
+rect_x = 100
+rect_y = 100
+speed_x = 2
+speed_y = 2
+rotation = 0
 
 screen = py.display.set_mode(SIZE)
 py.display.set_caption("PRISON MIKE BOUNCEHOUSE")
 
 clock = py.time.Clock()
+py.mixer.music.play(-1)
 
+done = False
 # Main Program Loop
-while True:
+while not done:
     # Event Loop
     for event in py.event.get():
         if event.type == py.QUIT:
             done = True
 
     # Game Logic -- Math
+    rect_x += speed_x
+    rect_y += speed_y
+    rotation += 1
+
+    if rect_y > 450 or rect_y < 0:
+        speed_y *= -1
+        bounceSound.play()
+    if rect_x > 560 or rect_x < 0:
+        speed_x *= -1
+        bounceSound.play()
 
     # Drawing Section
     screen.fill(BLACK)  # Always clear screen first
 
     # All your Drawing goes here
-    py.draw.rect(screen, RED, (100, 100, 200, 200))
+    rotatedMike = py.transform.rotate(resized_mike, rotation)
+    mike_rect = rotatedMike.get_rect(center = (rect_x +25, rect_y + 25))
+    screen.blit(rotatedMike, mike_rect.topleft)
     py.display.flip()
 
     clock.tick(60)  # FPS
