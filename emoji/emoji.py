6095922cb9839afcf379c76a7cda5fa6aa162e7a
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-15 10:45:48 -0700

    Add color constants and window settings to emoji module

--- emoji/emoji.py
+++ emoji/emoji.py
@@ -1,2 +1,12 @@
 import pygame as py
 
+# Colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+# Window Settings
+size = (700, 500)
+screen = py.display.set_mode(size)
