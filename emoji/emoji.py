4417c62fbe7a8cc85f511b8134deaef0464c34ff
Dragonlord1005 <dragonlord100587@gmail.com>
2025-01-15 10:57:41 -0700

    Remove Python version specification and project files; add VSCode settings for auto-import completions

--- emoji/emoji.py
+++ emoji/emoji.py
@@ -1,5 +1,6 @@
 import pygame as py
 
+py.init()
 # Colors
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
@@ -10,3 +11,7 @@ BLUE     = (   0,   0, 255)
 # Window Settings
 size = (700, 500)
 screen = py.display.set_mode(size)
+
+# Stuff
+done = False
+
