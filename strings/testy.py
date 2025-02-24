b361fb77996117480586468b7dee6a5f59e3d1af
Ryan Langston <dragonlord100587@gmail.com>
2025-02-24 10:06:33 -0700

    Refactor f-string examples in testy.py: comment out old prints and add new formatted output

--- strings/testy.py
+++ strings/testy.py
@@ -2,12 +2,12 @@ name = "Ryan"
 age = 37
 PI = 3.14159
 
-print(f"my name is {name} and I am {age} years old")
-print(f"my name is {name: <10} and I am {age} years old")
+# print(f"my name is {name} and I am {age} years old")
+# print(f"my name is {name: <10} and I am {age} years old")
 
-print(f"Pi rounded to 2 decimal places is: {PI:.2f}")
+# print(f"Pi rounded to 2 decimal places is: {PI:.2f}")
 
-print(f"Pi rounded to 4 decimal places is: {PI:.4f}")
+# print(f"Pi rounded to 4 decimal places is: {PI:.4f}")
 
 # F String Notes
 """
@@ -19,5 +19,10 @@ f before the string allows expressions/variables inside of the {}
 > aligns text to the right
 ^ centers the text
 
+To center and adjust alignment to make things look good we need to provide the WIDTH of the text
+
+
 
 """
+
+print(f"my name is {name:?<25} and I am {age} years old")
