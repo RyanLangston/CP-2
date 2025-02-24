7cd08e858d1291567b6e2e7819c20171a7bb9a32
Ryan Langston <dragonlord100587@gmail.com>
2025-02-24 10:01:50 -0700

    Add formatted print statements and documentation for f-strings in testy.py

--- strings/testy.py
+++ strings/testy.py
@@ -1,3 +1,23 @@
 name = "Ryan"
 age = 37
+PI = 3.14159
 
+print(f"my name is {name} and I am {age} years old")
+print(f"my name is {name: <10} and I am {age} years old")
+
+print(f"Pi rounded to 2 decimal places is: {PI:.2f}")
+
+print(f"Pi rounded to 4 decimal places is: {PI:.4f}")
+
+# F String Notes
+"""
+f before the string allows expressions/variables inside of the {}
+
+.2f 2 decimal places
+
+< aligns text to the left
+> aligns text to the right
+^ centers the text
+
+
+"""
