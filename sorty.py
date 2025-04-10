e53f66dbe8d97dc32a20bf7ffd3f4f1bfed509c0
Ryan Langston <dragonlord100587@gmail.com>
2025-04-10 10:10:27 -0600

    Refactor list generation and enhance timing for selection sort

--- sorty.py
+++ sorty.py
@@ -3,25 +3,33 @@ import time
 
 
 def calc_list(maxValue: int, howMany: int) -> list:
+    myList = []
     for i in range(howMany):
-        myList = []
         number = random.randint(0, maxValue)
         myList.append(number)
-        return myList
+
+    return myList
 
 
-nonSortedList = calc_list(8192, 3000)
+nonSortedList = calc_list(10000, 300000)
 
 
 def selection_sort(listToSort: list)  -> list:
     """Uses selection sort to sort a given list"""
-    # Copy the list to a new list
+    # Copy the list to a new list and track how long it takes to copy
+    start_time_to_copy = time.perf_counter()
     newList = listToSort.copy()
+    end_time_to_copy = time.perf_counter()
+    elapsed_time_to_copy = end_time_to_copy - start_time_to_copy
+
+    print(f"list took {elapsed_time_to_copy:.6f} seconds to copy")
+
     # shamelessly stolen from scaler.com, website included an explanation
     for i in range(len(newList)):
         # Set min_index = to first unsorted element
         min_index = i
 
+        start_time = time.perf_counter()
         # Loop to iterate over unsorted sub arrary
         for j in range(i + 1, len(newList)):
             # Finding the minimum element in the unsorted sub-array
@@ -29,12 +37,15 @@ def selection_sort(listToSort: list)  -> list:
                 min_index = j
 
         newList[i], newList[min_index] = newList[min_index], newList[i]
+    end_time = time.perf_counter()
 
-        # DEBUG: Following line is to be removed at a later date and is meant to be temp
-        print(newList)
+    elapsed_time = end_time - start_time
+    print(f"List took {end_time:.4f} seconds to sort")
 
-        return newList
+    return newList
 
+testList = selection_sort(nonSortedList)
+# print(testList)
 
 def linear_search(listToSort: list, target: int):
     """Given a list, searches it for a given target"""
@@ -61,11 +72,11 @@ def binary_search(list, targt):
     return -1
 
 
-target = int(input("What's the number? "))
+# target = int(input("What's the number? "))
 
-result = binary_search(nonSortedList, target)
+# result = binary_search(nonSortedList, target)
 
-if result == -1:
-    print(f"{target} not found in the list")
-else:
-    print(f"{target} found at index {result}")
+# if result == -1:
+#     print(f"{target} not found in the list")
+# else:
+#     print(f"{target} found at index {result}")
