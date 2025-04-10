648b5bf50c57d3f840ed510f9f7742ba94336cb3
Ryan Langston <dragonlord100587@gmail.com>
2025-04-10 09:36:17 -0600

    Refactor list generation and implement selection sort in sorty.py

--- sorty.py
+++ sorty.py
@@ -1,14 +1,40 @@
 import random
 import time
 
-def calcList(maxValue, howMany):
+def calc_list(maxValue: int, howMany: int) -> list:
     for i in range(howMany):
         myList = []
         number = random.randint(0, maxValue)
         myList.append(number)
         return myList
     
-myList = calcList
+nonSortedList = calc_list(8192, 3000)
+
+def selection_sort(listToSort: list):
+    # Copy the list to a new list
+    newList = listToSort.copy()
+    """Uses selection sort to sort a given list"""
+    # shamelessly stolen from scaler.com, website included an explanation
+    for i in range(len(newList)):
+
+        # Set min_index = to first unsorted element
+        min_index = i
+    
+        # Loop to iterate over unsorted sub arrary
+        for j in range(i+1, len(newList)):
+            # Finding the minimum element in the unsorted sub-array
+            if newList[min_index] > newList[j]:
+                min_index = j
+
+        newList[i], newList[min_index] = newList[min_index], newList[i]
+
+        # DEBUG: Following line is to be removed at a later date and is meant to be temp
+        print(newList)
+
+        return newList
+
+
+
 
 def linear_search(list, target):
     hits = []
@@ -34,7 +60,7 @@ def binary_search(list,targt):
 
 target = int(input("What's the number? "))
 
-result = binary_search(myList, target)
+result = binary_search(nonSortedList, target)
 
 if result == -1:
     print(f"{target} not found in the list")
