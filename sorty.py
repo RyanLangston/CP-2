4fd0a677bf55d6ef7e13fba01752e4c741b8ea2b
Ryan Langston <dragonlord100587@gmail.com>
2025-04-10 09:13:51 -0600

    Implement linear and binary search functions with random list generation

--- sorty.py
+++ sorty.py
@@ -1,2 +1,40 @@
 import random
+import time
 
+myList = []
+
+for i in range(1000):
+    number = random.randint(1, 1000)
+    myList.append(number)
+    myList.sort()
+
+def linear_search(list, target):
+    hits = []
+    for i in range(len(list)):
+        if list[i] == target:
+            hits.append(i)
+    return hits
+
+def binary_search(list,targt):
+    left = 0
+    right = len(list) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if list[mid] == target:
+            return mid
+        elif list[mid] < target:
+            left = mid + 1
+        else:
+            right = mid + 1
+    return -1
+
+target = int(input("What's the number? "))
+
+result = binary_search(myList, target)
+
+if result == -1:
+    print(f"{target} not found in the list")
+else:
+    print(f"{target} found at index {result}")
